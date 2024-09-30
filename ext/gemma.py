9f59b9a7c6504f3e3e263be30ac70e009ece9ea8
Nicolay Rusnachenko <kolyarus@yandex.ru>
2024-09-30 20:54:40 +0100

    Refactoring to `api_token`

--- ext/gemma.py
+++ ext/gemma.py
@@ -7,14 +7,14 @@ from bulk_chain.core.llm_base import BaseLM
 class Gemma(BaseLM):
 
     def __init__(self, model_name="google/gemma-7b-it", temp=0.1, device='cuda',
-                 max_length=None, token=None, use_bf16=False):
+                 max_length=None, api_token=None, use_bf16=False):
         super(Gemma, self).__init__(name=model_name)
         self.__device = device
         self.__max_length = 1024 if max_length is None else max_length
         self.__model = AutoModelForCausalLM.from_pretrained(
-            model_name, torch_dtype=torch.bfloat16 if use_bf16 else "auto", token=token)
+            model_name, torch_dtype=torch.bfloat16 if use_bf16 else "auto", token=api_token)
         self.__model.to(device)
-        self.__tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
+        self.__tokenizer = AutoTokenizer.from_pretrained(model_name, token=api_token)
         self.__temp = temp
 
     def ask(self, prompt):
