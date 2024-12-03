b59b4414b5df2d4e9dfca56891d2c2632b685b05
Nicolay Rusnachenko <rusnicolay@gmail.com>
2024-12-03 13:12:29 +0000

    #41 done, #40 related quick fix.

--- ext/openai_156.py
+++ ext/openai_156.py
@@ -1,18 +1,20 @@
+# This code has been tested under openai==1.56.0
+
+import logging
+
+from openai import OpenAI
 from bulk_chain.core.llm_base import BaseLM
-from bulk_chain.core.utils import auto_import
 
 
 class OpenAIGPT(BaseLM):
 
     def __init__(self, api_key, model_name="gpt-4-1106-preview", temp=0.1, max_tokens=None, assistant_prompt=None,
-                 freq_penalty=0.0, **kwargs):
-        assert(isinstance(assistant_prompt, str) or assistant_prompt is None)
-        super(OpenAIGPT, self).__init__(name=model_name, **kwargs)
+                 freq_penalty=0.0, attempts=None, suppress_httpx_log=True, **kwargs):
+        assert (isinstance(assistant_prompt, str) or assistant_prompt is None)
+        super(OpenAIGPT, self).__init__(name=model_name, attempts=attempts, **kwargs)
 
         # dynamic import of the OpenAI library.
-        OpenAI = auto_import("openai._client.OpenAI", is_class=False)
-
-        self.__client = OpenAI(api_key=api_key)
+        self.__client = OpenAI(api_key=api_key, base_url="https://api.openai.com/v1")
         self.__max_tokens = 256 if max_tokens is None else max_tokens
         self.__temperature = temp
         self.__model_name = model_name
@@ -20,12 +22,21 @@ class OpenAIGPT(BaseLM):
         self.__freq_penalty = freq_penalty
         self.__kwargs = {} if kwargs is None else kwargs
 
+        if "delimiter" in self.__kwargs:
+            del self.__kwargs["delimiter"]
+
+        if suppress_httpx_log:
+            httpx_logger = logging.getLogger("httpx")
+            httpx_logger.setLevel(logging.WARNING)
+
     def ask(self, prompt):
 
-        message = [
-            {"role": "assistant", "content": self.__assistant_prompt},
-            {"role": "user", "content": prompt}
-        ]
+        def __msg_content():
+            if self.__assistant_prompt is not None:
+                yield {"role": "assistant", "content": self.__assistant_prompt}
+            yield {"role": "user", "content": prompt}
+
+        message = list(__msg_content())
 
         response = self.__client.chat.completions.create(
             model=self.__model_name,
@@ -35,4 +46,5 @@ class OpenAIGPT(BaseLM):
             frequency_penalty=self.__freq_penalty,
             **self.__kwargs
         )
+
         return response.choices[0].message.content
