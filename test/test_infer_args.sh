93cd00aec9d7b0dfca0d0630cacddc0750c633f1
Nicolay Rusnachenko <rusnicolay@gmail.com>
2024-12-10 10:13:30 +0000

    #40 related unit test update

--- test/test_infer_args.sh
+++ test/test_infer_args.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:../
-python test_cmdargs.py %% --aaa 1 --b 3 --c 1.0 --key "sdflsdjfslfjsdlf"
+python test_cmdargs.py %%m --aaa 1 --b 3 --c 1.0 --key "sdflsdjfslfjsdlf" %%csv --m 234
