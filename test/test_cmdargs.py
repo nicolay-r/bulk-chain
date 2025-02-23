89c7dcbcacca1d412e431074d5f84cf2ee8eb16c
Nicolay Rusnachenko <kolyarus@yandex.ru>
2025-02-23 10:44:51 +0000

    Formatting cmdargs related test

--- test/test_cmdargs.py
+++ test/test_cmdargs.py
@@ -1,20 +1,29 @@
 import sys
+import unittest
 
 from bulk_chain.core.service_args import CmdArgsService
 
 
-# Csv-related.
-csv_args = CmdArgsService.find_grouped_args(sys.argv, starts_with="%%csv", end_prefix="%%")
-print(csv_args)
-csv_args = CmdArgsService.args_to_dict(csv_args)
-print("csv\t", csv_args)
+class TestCmdArgs(unittest.TestCase):
 
-# Model-related.
-m_args = CmdArgsService.find_grouped_args(sys.argv, starts_with="%%m", end_prefix="%%")
-m_args = CmdArgsService.args_to_dict(m_args)
-print("mod\t", m_args)
+    def test(self):
 
-# native.
-n_args = CmdArgsService.extract_native_args(sys.argv, end_prefix="%%")
-n_args = CmdArgsService.args_to_dict(n_args)
-print("nat\t", n_args)
+        # Csv-related.
+        csv_args = CmdArgsService.find_grouped_args(sys.argv, starts_with="%%csv", end_prefix="%%")
+        print(csv_args)
+        csv_args = CmdArgsService.args_to_dict(csv_args)
+        print("csv\t", csv_args)
+
+        # Model-related.
+        m_args = CmdArgsService.find_grouped_args(sys.argv, starts_with="%%m", end_prefix="%%")
+        m_args = CmdArgsService.args_to_dict(m_args)
+        print("mod\t", m_args)
+
+        # native.
+        n_args = CmdArgsService.extract_native_args(sys.argv, end_prefix="%%")
+        n_args = CmdArgsService.args_to_dict(n_args)
+        print("nat\t", n_args)
+
+
+if __name__ == '__main__':
+    unittest.main()
