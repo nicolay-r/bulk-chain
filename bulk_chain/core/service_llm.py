be8b8adc31a5e4dd84bb12b14ea9a5dff89778fc
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-04-04 11:38:39 +0100

    #77 related refactoring. Enhancing interaction and user experience.

--- bulk_chain/core/service_llm.py
+++ bulk_chain/core/service_llm.py
@@ -21,21 +21,18 @@ def chat_with_lm(lm, preset_dict=None, schema=None, model_name=None, pad=0):
     preset_dict = {} if preset_dict is None else preset_dict
 
     streamed_logger = StreamedLogger(__name__)
-
     do_exit = False
     model_name = model_name if model_name is not None else "agent"
 
     while not do_exit:
 
-        streamed_logger.info("----------------")
-        streamed_logger.info("\n")
-
         # Launching the CoT engine loop.
         data_dict = {} | preset_dict
 
         def callback_str_func(entry, info):
-            #streamed_logger.info(pad_str(f"{model_name} (resp [{chain_ind + 1}/{len(schema.chain)}])->", pad=pad))
+            streamed_logger.info(pad_str(f"{model_name} ({info['param']})->\n", pad=pad))
             streamed_logger.info(nice_output(entry, remove_new_line=False))
+            streamed_logger.info("\n\n")
 
         def handle_missed_value(col_name):
             user_input = input(f"Enter your prompt for `{col_name}`"
@@ -47,7 +44,13 @@ def chat_with_lm(lm, preset_dict=None, schema=None, model_name=None, pad=0):
             return user_input
 
         def callback_stream_func(entry, info):
-            #streamed_logger.info(pad_str(f"{model_name} (resp [{chain_ind + 1}/{len(schema.chain)}])->", pad=pad))
+            if entry is None and info["action"] == "start":
+                streamed_logger.info(pad_str(f"{model_name} ({info['param']})->\n", pad=pad))
+                return
+            if entry is None and info["action"] == "end":
+                streamed_logger.info("\n\n")
+                return
+
             streamed_logger.info(entry)
 
         content_it = iter_content(
@@ -62,6 +65,6 @@ def chat_with_lm(lm, preset_dict=None, schema=None, model_name=None, pad=0):
         )
 
         for _ in content_it:
-            user_input = input(f"Enter to continue (or 'exit' to quit) ...")
+            user_input = input(f"Enter to continue (or 'exit' to quit) ...\n")
             if user_input.lower() == 'exit':
                 do_exit = True
