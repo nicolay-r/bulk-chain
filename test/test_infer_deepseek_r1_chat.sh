eb6a69fb213503a0825d8cd0a02ca0196db62339
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-04-04 11:58:23 +0100

    Fixed links to schemas

--- test/test_infer_deepseek_r1_chat.sh
+++ test/test_infer_deepseek_r1_chat.sh
@@ -2,7 +2,7 @@
 export PYTHONPATH=$PYTHONPATH:../
 
 python ../bulk_chain/infer.py \
-    --schema "../schema/default.json" \
+    --schema "schema/default.json" \
     --adapter "dynamic:providers/replicate_104.py:Replicate" \
     %%m \
     --model_name "deepseek-ai/deepseek-r1" \
